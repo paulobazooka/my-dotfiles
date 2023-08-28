05785095fca0001297020f1110d581732a24ab5e
Paulo Sérgio do Nascimento <paulosergionas@gmail.com>
2023-08-27 23:35:46 -0300

    update nvim plugin and add zshrc

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -152,3 +152,27 @@ map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
 map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
 map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+
+
+-- Debug ---
+map("n", "<leader>dR", "<cmd>DapContinue<cr>", {desc = "Run to Cursor"}) -- function() require("dap").run_to_cursor() end, desc = "Run to Cursor", })
+--    { "<leader>dE", function() require("dapui").eval(vim.fn.input "[Expression] > ") end, desc = "Evaluate Input", },
+--    { "<leader>dC", function() require("dap").set_breakpoint(vim.fn.input "[Condition] > ") end, desc = "Conditional Breakpoint", },
+--    { "<leader>dU", function() require("dapui").toggle() end, desc = "Toggle UI", },
+--    { "<leader>db", function() require("dap").step_back() end, desc = "Step Back", },
+--    { "<leader>dc", function() require("dap").continue() end, desc = "Continue", },
+--    { "<leader>dd", function() require("dap").disconnect() end, desc = "Disconnect", },
+--    { "<leader>de", function() require("dapui").eval() end, mode = {"n", "v"}, desc = "Evaluate", },
+--    { "<leader>dg", function() require("dap").session() end, desc = "Get Session", },
+--    { "<leader>dh", function() require("dap.ui.widgets").hover() end, desc = "Hover Variables", },
+--    { "<leader>dS", function() require("dap.ui.widgets").scopes() end, desc = "Scopes", },
+--    { "<leader>di", function() require("dap").step_into() end, desc = "Step Into", },
+--    { "<leader>do", function() require("dap").step_over() end, desc = "Step Over", },
+--    { "<leader>dp", function() require("dap").pause.toggle() end, desc = "Pause", },
+--    { "<leader>dq", function() require("dap").close() end, desc = "Quit", },
+--    { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL", },
+--    { "<leader>ds", function() require("dap").continue() end, desc = "Start", },
+--    { "<leader>dt", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint", },
+--    { "<leader>dx", function() require("dap").terminate() end, desc = "Terminate", },
+--    { "<leader>du", function() require("dap").step_out() end, desc = "Step Out", },
+--  },
