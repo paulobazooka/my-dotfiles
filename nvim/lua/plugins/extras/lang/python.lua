afe211f04523a579c3151c6aeca60c3c95bfd247
Paulo Sérgio do Nascimento <pasergio@unicamp.br>
2023-10-11 12:10:59 -0300

    update

--- nvim/lua/plugins/extras/lang/python.lua
+++ nvim/lua/plugins/extras/lang/python.lua
@@ -44,6 +44,19 @@ return {
       setup = {
         debugpy = function(_, _)
           require("dap-python").setup("python", {})
+          require("dap-python").resolve_python = function()
+            -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+            local cwd = vim.fn.getcwd()
+            if vim.fn.executable(cwd .. "./venv/bin/python") == 1 then
+              return cwd .. "/venv/bin/python"
+            elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+              return cwd .. "/.venv/bin/python"
+            else
+              return "/usr/bin/python"
+            end
+          end
           table.insert(require("dap").configurations.python, {
             {
               type = "python",
@@ -71,6 +84,7 @@ return {
               request = "launch",
               name = "Launch file",
               program = "${file}",
+              module = "uvicorn",
               pythonPath = function()
                 -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
                 -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
