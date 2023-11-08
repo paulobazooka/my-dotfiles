92c3f5b0cba3f9aee5e45b110a76cb6b1b2c794d
Paulo Sérgio do Nascimento <paulosergionas@gmail.com>
2023-11-07 22:03:49 -0300

    update

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,6 +1,8 @@
 return {
   {
     "norcalli/nvim-colorizer.lua",
-    lazy = false
-  }
+    lazy = false,
+    keys = { { "<leader>cc", "<cmd>ColorizerToggle<cr>", desc = "Colorize Toggle" } },
+    config = function() end,
+  },
 }
