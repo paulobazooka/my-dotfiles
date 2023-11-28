2832ae63e6df9daff39fba72bb47252d6cddbfac
Paulo Sérgio do Nascimento <pasergio@unicamp.br>
2023-11-28 15:20:58 -0300

    update

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -3,8 +3,12 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     lazy = true,
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    keys = { { "<leader>cp", "<cmd>MarkdownPreviewToggle<cr>", desc = "  Markdown Preview Toggle" } },
     config = function()
       vim.fn["mkdp#util#install"]()
     end,
   },
 }
+
