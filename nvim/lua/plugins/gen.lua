e58ff2d2ab3c8d49345de97973ab84aa1d2cfa3b
Paulo Sérgio do Nascimento <pasergio@unicamp.br>
2024-03-06 10:52:36 -0300

    add gen ollama

--- nvim/lua/plugins/gen.lua
+++ nvim/lua/plugins/gen.lua
@@ -2,10 +2,10 @@
 return {
   "David-Kunz/gen.nvim",
   opts = {
-    model = "codellama",    -- The default model to use.
+    model = "mistral",      -- The default model to use.
     host = "localhost",     -- The host running the Ollama service.
     port = "11434",         -- The port on which the Ollama service is listening.
-    display_mode = "split", -- The display mode. Can be "float" or "split".
+    display_mode = "float", -- The display mode. Can be "float" or "split".
     show_prompt = false,    -- Shows the Prompt submitted to Ollama.
     show_model = true,      -- Displays which model you are using at the beginning of your chat session.
     no_auto_close = false,  -- Never closes the window automatically.
@@ -23,10 +23,26 @@ return {
     debug = false -- Prints errors and the command which is run.
   },
   config = function()
-    require("gen").prompts["Codar"] = {
+    local prompts = require("gen").prompts
+
+    prompts["Developer"] = {
+      mmodel = "codellama",
+      display_mode = "float", -- The display mode. Can be "float" or "split".
       prompt =
-      "Você é um desenvolvedor fullstack, com conhecimento em boas práticas de programação, arquitetura e engenharia de software. Irá auxiliar a melhorar o código promovendo melhorias e exemplos.\n$text",
-      replace = true,
+      "Você é um desenvolvedor fullstack, com conhecimento em boas práticas de programação, arquitetura e engenharia de software. Irá auxiliar a melhorar o código promovendo melhorias e provendo exemplos de código para ser inserido.\n$text",
+      replace = false,
+      show_model = true,     -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = false, -- Never closes the window automatically.
+    }
+
+    prompts["Analise de Texto"] = {
+      model = "llama2",
+      display_mode = "float", -- The display mode. Can be "float" or "split".
+      prompt =
+      "Você está no papel de um interpretador de textos em português, deverá ler o texto e sintetizar o conteúdo em uma análise de sentimento sobre o que o texto lido. O resumo deverá ser na lingua portuguesa. O resumo deverá ser feito na lingua portuguesa e conter até 200 palavras.$input\n$text",
+      replace = false,
+      show_model = true,     -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = false, -- Never closes the window automatically.
     }
   end,
 }
