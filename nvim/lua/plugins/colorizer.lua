9243e929a440dcdd64ab36bc14390f00dd8b20fb
Paulo Sérgio do Nascimento <pasergio@unicamp.br>
2024-04-18 13:43:23 -0300

    update

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -2,7 +2,7 @@ return {
   {
     "norcalli/nvim-colorizer.lua",
     lazy = false,
-    keys = { { "<leader>cc", "<cmd>ColorizerToggle<cr>", desc = "Colorize Toggle" } },
+    keys = { { "<leader>cz", "<cmd>ColorizerToggle<cr>", desc = "Colorize Toggle" } },
     config = function() end,
   },
 }
