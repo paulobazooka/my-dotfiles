7af8f008bedd2f641986d9e9f0df2fc419deae60
Paulo Sérgio do Nascimento <paulosergionas@gmail.com>
2024-09-07 10:28:02 -0300

    update

--- nvim/lua/plugins/gen.lua
+++ nvim/lua/plugins/gen.lua
@@ -2,15 +2,17 @@
 return {
   "David-Kunz/gen.nvim",
   opts = {
-    model = "mistral",      -- The default model to use.
-    host = "localhost",     -- The host running the Ollama service.
-    port = "11434",         -- The port on which the Ollama service is listening.
+    model = "mistral", -- The default model to use.
+    host = "localhost", -- The host running the Ollama service.
+    port = "11434", -- The port on which the Ollama service is listening.
     display_mode = "float", -- The display mode. Can be "float" or "split".
-    show_prompt = false,    -- Shows the Prompt submitted to Ollama.
-    show_model = true,      -- Displays which model you are using at the beginning of your chat session.
-    no_auto_close = false,  -- Never closes the window automatically.
+    show_prompt = false, -- Shows the Prompt submitted to Ollama.
+    show_model = true, -- Displays which model you are using at the beginning of your chat session.
+    no_auto_close = false, -- Never closes the window automatically.
     prompt = "",
-    init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
+    init = function(options)
+      pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+    end,
     -- Function to initialize Ollama
     command = function(options)
       return "curl --silent --no-buffer -X POST http://" .. options.host .. ":" .. options.port .. "/api/chat -d $body"
@@ -20,7 +22,7 @@ return {
     -- The executed command must return a JSON object with { response, context }
     -- (context property is optional).
     -- list_models = '<omitted lua function>', -- Retrieves a list of model names
-    debug = false -- Prints errors and the command which is run.
+    debug = false, -- Prints errors and the command which is run.
   },
   config = function()
     local prompts = require("gen").prompts
@@ -28,20 +30,18 @@ return {
     prompts["Developer"] = {
       mmodel = "codellama",
       display_mode = "float", -- The display mode. Can be "float" or "split".
-      prompt =
-      "Você é um desenvolvedor fullstack, com conhecimento em boas práticas de programação, arquitetura e engenharia de software. Irá auxiliar a melhorar o código promovendo melhorias e provendo exemplos de código para ser inserido.\n$text",
+      prompt = "Você é um desenvolvedor fullstack, com conhecimento em boas práticas de programação, arquitetura e engenharia de software. Irá auxiliar a melhorar o código promovendo melhorias e provendo exemplos de código para ser inserido.\n$text",
       replace = false,
-      show_model = true,     -- Displays which model you are using at the beginning of your chat session.
+      show_model = true, -- Displays which model you are using at the beginning of your chat session.
       no_auto_close = false, -- Never closes the window automatically.
     }
 
     prompts["Analise de Texto"] = {
       model = "llama2",
       display_mode = "float", -- The display mode. Can be "float" or "split".
-      prompt =
-      "Você está no papel de um interpretador de textos em português, deverá ler o texto e sintetizar o conteúdo em uma análise de sentimento sobre o que o texto lido. O resumo deverá ser na lingua portuguesa. O resumo deverá ser feito na lingua portuguesa e conter até 200 palavras.$input\n$text",
+      prompt = "Você está no papel de um interpretador de textos em português, deverá ler o texto e sintetizar o conteúdo em uma análise de sentimento sobre o que o texto lido. O resumo deverá ser na lingua portuguesa. O resumo deverá ser feito na lingua portuguesa e conter até 200 palavras.$input\n$text",
       replace = false,
-      show_model = true,     -- Displays which model you are using at the beginning of your chat session.
+      show_model = true, -- Displays which model you are using at the beginning of your chat session.
       no_auto_close = false, -- Never closes the window automatically.
     }
   end,
